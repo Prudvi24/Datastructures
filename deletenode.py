3ec2d58a99c9be1b88b9bf534a52869d2b747287
Prudvi24 <39848170+Prudvi24@users.noreply.github.com>
2018-06-05 12:09:42 +0530

    delete_node

---
+++ deletenode.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        
+    def push(self,new_data):
+        new_node=Node(new_data)
+        new_node.next=self.head
+        self.head=new_node
+        
+    def deleteNode(self,key):
+        temp=self.head
+
+        if temp is not None:
+            if temp.data==key:
+                self.head=temp.next
+                temp=None
+                return
+        
+        while temp is not None:
+            if temp.data==key:
+                break
+            prev = temp
+            temp = temp.next
+
+        if temp==None:
+            return
+        if temp is not None:
+            prev.next=temp.next
+            temp=None
+            
+    def printList(self):
+        temp=self.head
+        while(temp):
+            print temp.data
+            temp=temp.next
+
+
+lis = LinkedList()
+lis.push(7)
+lis.push(1)
+lis.push(3)
+lis.push(2)
+lis.deleteNode(1)
+lis.printList()
+
+
+
+
+
+
+
+
+
+
+            
